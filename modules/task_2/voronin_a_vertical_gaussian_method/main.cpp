0b882840e98a6e7a3f828a56dd09806ecc83536e
VoroninAA <voralexan@gmail.com>
2020-11-05 16:43:15 -0500

    sequential version

---
+++ modules/task_2/voronin_a_vertical_gaussian_method/main.cpp
@@ -0,0 +1,73 @@
+// Copyright 2020 Voronin Aleksey
+#include <gtest-mpi-listener.hpp>
+#include <gtest/gtest.h>
+#include <vector>
+#include "./vertical_gaussian_method.h"
+
+TEST(Parallel_Operations_MPI, can_get_result_with_sequential_version) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0) {
+        const int unknownNumber = 3;
+        std::vector<double> sampleMatrix = {2,1,1,2,
+                                           1,-1,0,-2,
+                                           3,-1,2,2};
+        std::vector<double> result = sequentialGaussianMethod(sampleMatrix,unknownNumber);
+        std::vector<double> expectedResult = {-1,1,3};
+        ASSERT_EQ(result, expectedResult);
+    }
+}
+
+TEST(Parallel_Operations_MPI, can_get_result_with_sequential_version_two_unknowns) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0) {
+        const int unknownNumber = 2;
+        std::vector<double> sampleMatrix = {1,-1,-5,
+                                            2,1,-7};
+        std::vector<double> result = sequentialGaussianMethod(sampleMatrix,unknownNumber);
+        std::vector<double> expectedResult = {-4,1};
+        ASSERT_EQ(result, expectedResult);
+    }
+}
+
+TEST(Parallel_Operations_MPI, cant_get_result_with_wrong_input) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0) {
+        const int unknownNumber = 4;
+        std::vector<double> sampleMatrix = {1,-1,-5,
+                                            2,1,-7};
+        ASSERT_ANY_THROW(std::vector<double> result = sequentialGaussianMethod(sampleMatrix,unknownNumber));
+    }
+}
+
+TEST(Parallel_Operations_MPI, can_get_result_with_parallel_version_two_unknowns) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0) {
+        const int unknownNumber = 2;
+        std::vector<double> sampleMatrix = {1,-1,-5,
+                                            2,1,-7};
+        ASSERT_NO_THROW(std::vector<double> result = parallelGaussianMethod(sampleMatrix,unknownNumber));
+    }
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
+
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
+}
